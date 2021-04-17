9658e227bb3a51c06fa48bc6f7381313c9b12f21
benvonhep <benvonhep@gmail.com>
2021-04-17 08:00:08 +0200

    added casual and fancy filter to filtercomponent

--- loc-fin-2020/src/components/layout/FilterModal.jsx
+++ loc-fin-2020/src/components/layout/FilterModal.jsx
@@ -8,67 +8,34 @@ import { Button, Form, Modal } from 'react-bootstrap';
 import './FilterModal.scss'
 
 const  FilterModal = (props)=> {
-  // const {filterCategories, setFilterCategories } = useState([
-  //   {
-  //     id: 1,
-  //     name: 'breakfast',
-  //     value: 'breakfast'
-  //   },
-  //   {
-  //     id: 2,
-  //     name: 'lunch',
-  //     value: 'lunch'
-  //   },
-  //   {
-  //     id: 3,
-  //     name: 'dinner',
-  //     value: 'dinner'
-  //   }
-  // ])
 
   const { activeFilter, setActiveFilter, filterCategories, testlog, onHide, ...rest} = props;
-  // const [ activeFilter, setActiveFilter ] = useState([])
   const onCancel = () => {
     onHide()
     // dispatch(resetLocation())
   }
 
   const onFilterChange = (filter) => {
-    // if(activeFilter){ console.log(Object.entries(activeFilter).length, 'AF') }
-    // if(activeFilter){console.log(Object.keys(activeFilter), 'AFK')}
-    // if(activeFilter){console.log(Object.values(activeFilter), 'AFV')}
-    // console.log(filterCategories.length, 'FCL');
-    // console.log(filter, 'Filter');
     let filterCatMAP = Object.entries(filterCategories);
     if (filter === 'ALL') {
-      // console.log('All clicked');
       if ( activeFilter && Object.entries(activeFilter).length === filterCategories.length){
-        // console.log('all have been selected');
         setActiveFilter([])
       } else {
-        // console.log('new filter aded');
         let newFilter = filterCatMAP.map(item => item[1].value)
         setActiveFilter(newFilter)
       }
     } else {
-      // console.log('others clicked');
-
 
       let activeEntries = activeFilter ? Object.values(activeFilter).find(obj => {
-        // console.log(obj.value, 'objval')
-        // console.log(filter.value, 'filterval')
-        // console.log(obj === filter, '===');
         if (obj === filter) return true;
       }) : [];
+
       if (activeEntries) {
-        // console.log('object entries ...');
         const filterIndex = activeFilter ? activeFilter.indexOf(filter) : undefined || null;
         const newFilter = [...activeFilter];
         newFilter.splice(filterIndex, 1);
         setActiveFilter(newFilter);
-        // console.log(newFilter);
       } else {
-        // console.log('NO object entries ...');
         if(activeFilter){setActiveFilter([...activeFilter, filter])}
       }
     }

8c7dac8b7d92d856ac1e6a46587eb5cda49caad2
benvonhep <benvonhep@gmail.com>
2022-03-19 22:15:38 +0100

    add comment to auth0

--- loc-fin-2020/src/components/layout/SignupButton.jsx
+++ loc-fin-2020/src/components/layout/SignupButton.jsx
@@ -1,21 +1,20 @@
-import React from "react";
-import { useAuth0 } from "@auth0/auth0-react";
-import { Button } from "react-bootstrap";
-
+import React from 'react';
+import { useAuth0 } from '@auth0/auth0-react';
+import { Button } from 'react-bootstrap';
 
 const SignupButton = () => {
   const { loginWithRedirect } = useAuth0();
 
   const signup = () => {
+    //todo -> check if mailadress is on allowed list before signup
     loginWithRedirect({
-      screen_hint: "signup",
-    })
-  }
+      screen_hint: 'signup',
+    });
+  };
 
   return (
     <Button
-      onClick={() => signup()
-      }
+      onClick={() => signup()}
       variant="outline-secondary"
       className="mt-2"
       size="sm"
